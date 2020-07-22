7c228882ed3b37b5791d6ead8eec4e7a00ef4f16
Rookie <fengwensheng123@163.com>
2020-07-22 17:00:09 +0800

    上传项目

---
+++ src/main/java/com/example/testpipeline/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.example.testpipeline.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: Rookie
+ * @Date: Create in 2020/07/22 16:53
+ * @Description:
+ **/
+@RestController
+@RequestMapping("test")
+public class TestController {
+
+    @GetMapping("hello")
+    public String hello(){
+        return "hello, world!";
+    }
+}
